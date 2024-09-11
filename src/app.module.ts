618ef7a6e4e2c055cfbb78b2a55eeb5ec8755acd
isra <isramartin157@gmail.com>
2024-09-10 22:06:34 -0600

    feat: agregar agenda

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MedicamentoModule } from './medicamento/medicamento.module';
+import { AgendaModule } from './agenda/agenda.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, MedicamentoModule],
+  imports: [UsersModule, AuthModule, MedicamentoModule, AgendaModule],
   controllers: [AppController],
   providers: [AppService],
 })
