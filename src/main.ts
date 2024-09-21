c9c157f1251897270b6c14d4329e663cf00c557f
isra <isramartin157@gmail.com>
2024-09-21 00:41:42 -0600

    ajustes de cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   const configService = app.get(ConfigService);
   const apiVersion = configService.get<string>('API_VERSION') || 'v1';
    // Configura el prefijo global
    app.setGlobalPrefix(apiVersion);
+ 
+   app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
